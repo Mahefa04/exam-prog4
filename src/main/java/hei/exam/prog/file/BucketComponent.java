e299e05d93eac47297a01fdf82f410f2902bdc18
Mahefa04 <hei.mahefa.2@gmail.com>
2026-07-13 09:45:32 +0300

    fix: resolve bugs in SubmissionController

--- src/main/java/hei/exam/prog/file/BucketComponent.java
+++ src/main/java/hei/exam/prog/file/BucketComponent.java
@@ -1,5 +1,8 @@
 package hei.exam.prog.file;
 
+import java.io.File;
+import java.net.URL;
+import java.time.Duration;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -7,37 +10,32 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
-import java.io.File;
-import java.net.URL;
-import java.time.Duration;
-
 @Component
 public class BucketComponent {
 
-    private final S3Client s3Client;
-    private final S3Presigner s3Presigner;
-    private final String bucketName = "votre-nom-de-bucket-s3"; // À adapter ou lier à une property
+  private final S3Client s3Client;
+  private final S3Presigner s3Presigner;
+  private final String bucketName = "votre-nom-de-bucket-s3"; // À adapter ou lier à une property
 
-    public BucketComponent(S3Client s3Client, S3Presigner s3Presigner) {
-        this.s3Client = s3Client;
-        this.s3Presigner = s3Presigner;
-    }
+  public BucketComponent(S3Client s3Client, S3Presigner s3Presigner) {
+    this.s3Client = s3Client;
+    this.s3Presigner = s3Presigner;
+  }
 
-    public void upload(File file, String key) {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-        s3Client.putObject(putObjectRequest, file.toPath());
-    }
+  public void upload(File file, String key) {
+    PutObjectRequest putObjectRequest =
+        PutObjectRequest.builder().bucket(bucketName).key(key).build();
+    s3Client.putObject(putObjectRequest, file.toPath());
+  }
 
-    public URL presign(String key, Duration expiration) {
-        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
-                .signatureDuration(expiration)
-                .getObjectRequest(builder -> builder.bucket(bucketName).key(key).build())
-                .build();
+  public URL presign(String key, Duration expiration) {
+    GetObjectPresignRequest presignRequest =
+        GetObjectPresignRequest.builder()
+            .signatureDuration(expiration)
+            .getObjectRequest(builder -> builder.bucket(bucketName).key(key).build())
+            .build();
 
-        PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
-        return presignedRequest.url();
-    }
+    PresignedGetObjectRequest presignedRequest = s3Presigner.presignGetObject(presignRequest);
+    return presignedRequest.url();
+  }
 }
