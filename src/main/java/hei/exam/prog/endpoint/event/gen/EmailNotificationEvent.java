f3b6b4eb9bde379991b36722e8a5c01edc918d1e
Mahefa04 <hei.mahefa.2@gmail.com>
2026-07-13 09:02:35 +0300

    feat: add class emailNotificationEvent

--- src/main/java/hei/exam/prog/endpoint/event/gen/EmailNotificationEvent.java
+++ src/main/java/hei/exam/prog/endpoint/event/gen/EmailNotificationEvent.java
@@ -1,4 +1,22 @@
 package hei.exam.prog.endpoint.event.gen;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmailNotificationEvent {
+    @JsonProperty("submission_id")
+    private String submissionId;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("file_name")
+    private String fileName;
 }
